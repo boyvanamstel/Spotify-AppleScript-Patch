025a1422b890ed7f9a447d9ed7ddf77603d1c01c
Boy van Amstel <boy@boyvanamstel.nl>
2015-03-10 13:43:08 +0100

    Initial Commit

---
+++ Spotify AppleScript Patch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spotify AppleScript Patch
+//
+//  Created by Boy van Amstel on 10/03/15.
+//  Copyright (c) 2015 Danger Cove. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
